3e4410a69a457c350fa55a8cc66a678063e1a891
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2022-09-13 12:20:44 +0200

    added docker compose and checks to actions

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y an return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subract x from y and return result."""
+    return y - x
