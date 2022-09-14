6131c40f212a8468aa6ea06e265a17c432b5fa0b
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2022-09-14 14:21:53 +0200

    checking code for failed test

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,8 +18,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +26,8 @@ urlpatterns = [
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs'
+        name='api-docs',
     ),
+    path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
